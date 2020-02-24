1f35fbb820b0dbc8a9bf973ee4e00758acc16036
Samuel Martinez <semartinez147@gmail.com>
2020-02-24 07:58:02 -0700

    Wrote unit tests.  Still working on General McNugget code.

--- src/edu/cnm/deepdive/Frobenius.java
+++ src/edu/cnm/deepdive/Frobenius.java
@@ -5,18 +5,59 @@ public class Frobenius {
   /*  Pack sizes are 6, 9, and 20*/
   public static boolean isMcNugget(int value) {
 
-    /*    TODO return true if value can be expressed as the sum of McNugget packs
-           (integral multiples including zero of the 3 original pack sizes)*/
-    if (value <0) {
+    return value >= 0 && (value == 0
+        || isMcNugget(value - 6)
+        || isMcNugget(value - 9)
+        || isMcNugget(value - 20)
+    );
+  }
+
+
+  /**
+   * Return true if the stated value can be formed as a sum of non-negative integral multiples of
+   * the elements of packSizes.
+   *
+   * @param value     target/goal number.
+   * @param packSizes array of distinct, positive pack sizes, in descending order.
+   * @return true if value is a McNugget number using the specific pack sizes, false otherwise.
+   */
+  public static boolean isNotGeneralMcNugget(int value, int[] packSizes) {
+    boolean flag = false;
+    if (packSizes.length == 1 || packSizes.length == 0) {
+      if (value >= 0 && value == 0 || isGeneralMcNugget(value - packSizes[0], null));
+      flag = true;
+    }
+    for (int i = 0; i < packSizes.length; i++) {
+      int[] ithElementArray = {packSizes[i]};
+      isGeneralMcNugget(value, ithElementArray);
+    }
+    return flag;
+  }
+
+  public static boolean isGeneralMcNugget(int value, int[] packSizes) {
+    if (value < 0) {
       return false;
     }
     if (value == 0) {
       return true;
     }
-    return (isMcNugget(value-6)
-        || isMcNugget(value-9)
-        || isMcNugget(value-20)
-        );
+    if (value > 0) {
+      for (int i = 0; i < packSizes.length; i++) {
+        switch (Integer.signum((value - packSizes[i]))) {
+          case 0:
+            return true;
+          case -1:
+            break;
+          case 1:
+            int[] jthElementArray = new int[packSizes.length];
+            for (int j = 0; j < packSizes.length; j++) {
+                jthElementArray[j] = value -
+                isGeneralMcNugget(value, jthElementArray);
+              }
+        }
+      }
+    }
+    return false;
   }
 
 }
--- test/edu/cnm/deepdive/FrobeniusTest.java
+++ test/edu/cnm/deepdive/FrobeniusTest.java
@@ -2,15 +2,49 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class FrobeniusTest {
+
+  private static Stream<Arguments> provideDataForGeneralMcNuggetAffirmative() {
+    return Stream.of(
+        Arguments.of(29, new int[] {20, 9, 2})/*,
+        Arguments.of(30, new int[] {11, 7, 5}),
+        Arguments.of(37, new int[] {11, 7, 5}),
+        Arguments.of(216, new int[] {50, 8}),
+        Arguments.of(98, new int[] {50, 8})*/
+    );
+  }
+  
+  private static Stream<Arguments> provideDataForGeneralMcNuggetNegative() {
+    return Stream.of(
+        Arguments.of(19, new int[] {20, 9, 2}),
+        Arguments.of(10, new int[] {11, 7, 5}),
+        Arguments.of(16, new int[] {11, 7, 5}),
+        Arguments.of(115, new int[] {50, 8}),
+        Arguments.of(201, new int[] {50, 8})
+    );
+  }
 
   @ParameterizedTest
   @CsvFileSource(resources = "mcnugget-data.csv", numLinesToSkip = 1)
     void isMcNugget(int value, boolean expected) {
   assertEquals(expected, Frobenius.isMcNugget(value));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideDataForGeneralMcNuggetAffirmative")
+  void isGeneralMcNugget_affirmative(int value, int[] packSizes) {
+    assertTrue(Frobenius.isGeneralMcNugget(value, packSizes));
+  }
+  @ParameterizedTest
+  @MethodSource("provideDataForGeneralMcNuggetNegative")
+  void isGeneralMcNugget(int value, int[] packSizes) {
+    assertFalse(Frobenius.isGeneralMcNugget(value, packSizes));
   }
 }
